8fc7dd0c8873fa3b352762715f8900c3d93e532d
shin <16229439+shin11788@user.noreply.gitee.com>
2025-12-08 12:37:40 +0800

    去除底部导航栏的水波纹效果

--- app/src/main/java/com/shin/vicmusic/core/design/component/MyNavigationBar.kt
+++ app/src/main/java/com/shin/vicmusic/core/design/component/MyNavigationBar.kt
@@ -4,6 +4,7 @@ import android.R.attr.contentDescription
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -42,12 +44,16 @@ fun MyNavigationBar(
             val color = if (selected)
                 MaterialTheme.colorScheme.primary
             else MaterialTheme.colorScheme.onSurface
+            val interactionSource = remember { MutableInteractionSource() }
             Column(
                 modifier = Modifier
                     .weight(1f)
                     .padding(vertical = SpaceExtraMedium)
                     //.navigationBarsPadding()
-                    .clickable {
+                    .clickable(
+                        indication = null,
+                        interactionSource = interactionSource,
+                    ){
                         onNavigateToDestination(index)
                     },
                 horizontalAlignment = Alignment.CenterHorizontally
