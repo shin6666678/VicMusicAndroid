41dab8cd926e57d9d37c8e7e577a9cd5cba7f9d8
shin <16229439+shin11788@user.noreply.gitee.com>
2025-12-17 13:17:20 +0800

    feat(vip): 提取并复用VIP标签组件

---
+++ app/src/main/java/com/shin/vicmusic/feature/vip/VipIcon.kt
@@ -0,0 +1,33 @@
+package com.shin.vicmusic.feature.vip
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun VipIcon(
+    vipTagText: String,
+    vipBgColor: Color,
+    vipTextColor: Color
+){
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(4.dp))
+            .background(vipBgColor)
+            .padding(horizontal = 6.dp, vertical = 2.dp)
+    ) {
+        Text(
+            text = vipTagText, // 动态文本
+            style = MaterialTheme.typography.labelSmall,
+            color = vipTextColor // 动态颜色
+        )
+    }
+}
